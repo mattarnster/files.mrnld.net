1f6c316d26245eacd4a5ecb5781826f3a4b12f31
Matthew Arnold <mattarnster.co.uk@gmail.com>
2021-03-22 08:20:55 -0400

    Create & password-protect libraries

--- routes/libraries.js
+++ routes/libraries.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const nanoid = require('nanoid')
+const bcrypt = require('bcrypt')
 const { getDb } = require('../db')
+const { OPEN_READWRITE } = require('sqlite3')
 
 var router = express.Router()
 
@@ -9,9 +11,9 @@ var db = getDb()
 router.get('/', async (req, res) => {
     if (!req.session.logged_in) {
         req.flash('failure', 'You must log in to perform this operation.')
-        res.redirect('/')
+        return res.redirect('/')
     }
-    var stmt = db.prepare('SELECT rowid,name,privateId FROM libraries WHERE userId=?')
+    var stmt = db.prepare('SELECT rowid,name,privateId,private,allowUploads FROM libraries WHERE userId=?')
     stmt.all(req.session.user_id, (err, rows) => {
         if (rows.length === 0) {
             return res.redirect('/libraries/new')
@@ -24,15 +26,15 @@ router.get('/', async (req, res) => {
 router.get('/new', async (req, res) => {
     if (!req.session.logged_in) {
         req.flash('failure', 'You must log in to perform this operation.')
-        res.redirect('/')
+        return res.redirect('/')
     }
-    return res.render('new-library', { csrfToken: req.csrfToken() })
+    return res.render('new-library')
 })
 
 router.post('/', async (req, res) => {
     if (!req.session.logged_in) {
         req.flash('failure', 'You must log in to perform this operation.')
-        res.redirect('/')
+        return res.redirect('/')
     }
 
     if (req.body.library_name === "") {
@@ -40,15 +42,25 @@ router.post('/', async (req, res) => {
         return res.redirect('/libraries')
     }
 
-    var stmt = db.prepare('SELECT name from libraries WHERE userId=?')
-    stmt.run(req.session.user_id, (err, rows) => {
-        if (rows) {
+    if (req.body.private_library && req.body.password === "") {
+        req.flash('failure', 'You must enter a password for a private library')
+        return res.redirect('/libraries/new')
+    }
+
+    var stmt = db.prepare('SELECT name from libraries WHERE userId=? AND name=?')
+    stmt.all(req.session.user_id, req.body.library_name, (err, rows) => {
+        if (rows.length > 0) {
             req.flash('failure', 'The new library must have a unique name.')
             return res.redirect('/libraries')
         }
+        
+        var hashedPassword = ""
+        if (req.body.private_library && req.body.password) {
+            hashedPassword = bcrypt.hashSync(req.body.password, 12)
+        }
 
-        var stmt = db.prepare('INSERT INTO libraries VALUES (?,?,?,?)')
-        stmt.run(null, req.session.user_id, req.body.library_name, nanoid.nanoid(), (err, row) => {
+        var stmt = db.prepare('INSERT INTO libraries VALUES (?,?,?,?,?,?,?)')
+        stmt.run(null, req.session.user_id, req.body.library_name, nanoid.nanoid(), req.body.private_library ? 1 : 0, req.body.allow_uploads ? 1 : 0, hashedPassword, (err, row) => {
             if (err) {
                 req.flash('failure', 'Error creating library: ' + err)
                 return res.redirect('/libraries')
@@ -63,15 +75,57 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     if (!req.session.logged_in) {
         req.flash('failure', 'You must log in to perform this operation.')
-        res.redirect('/')
+        return res.redirect('/')
     }
+
     var libraryName = "" 
-    db.get('SELECT rowid, name, userId FROM libraries WHERE rowid=? AND userId=?', req.params.id, req.session.user_id, (err, row) => {
+    var isPrivate = false
+    db.get('SELECT rowid, name, userId, private FROM libraries WHERE rowid=? AND userId=?', req.params.id, req.session.user_id, (err, row) => {
         libraryName = row.name
-    })
-    var stmt = db.prepare('SELECT * FROM libraries JOIN uploads ON uploads.libraryId=libraries.rowid WHERE libraries.rowid=? AND uploads.userId=?;')
-    stmt.all(req.params.id, req.session.user_id, (err, rows) => {
-        return res.render('view-library', { files: rows, libraryName: libraryName })
+        isPrivate = (row.private === 1 ? true : false)
+
+        if (isPrivate) {
+            return res.render('library-password', { library: row })
+        }
+    
+        var stmt = db.prepare('SELECT * FROM libraries JOIN uploads ON uploads.libraryId=libraries.rowid WHERE libraries.rowid=? AND uploads.userId=?;')
+        stmt.all(req.params.id, req.session.user_id, (err, rows) => {
+            return res.render('view-library', { files: rows, libraryName: libraryName })
+        })
+    }) 
+})
+
+router.post('/:id', async (req, res) => {
+    if (!req.session.logged_in) {
+        req.flash('failure', 'You must log in to perform this operation.')
+        return res.redirect('/')
+    }
+
+    if (!req.body.password || req.body.password === '') {
+        req.flash('failure', 'You must enter the libary password')
+        return res.redirect('/libraries/' + req.params.id)
+    }
+
+    db.get('SELECT rowid,password,userId FROM libraries WHERE rowid=? AND userId=?', req.params.id, req.session.user_id, (err, row) => {
+        if (row) {
+            console.log(row)
+            bcrypt.compare(req.body.password, row.password, (err, same) => {
+                if (same) {
+                    db.get('SELECT rowid, name, userId, private FROM libraries WHERE rowid=? AND userId=?', req.params.id, req.session.user_id, (err, row) => {
+                        libraryName = row.name
+                        isPrivate = (row.private === 1 ? true : false)
+                
+                        var stmt = db.prepare('SELECT * FROM libraries JOIN uploads ON uploads.libraryId=libraries.rowid WHERE libraries.rowid=? AND uploads.userId=?;')
+                        stmt.all(req.params.id, req.session.user_id, (err, rows) => {
+                            return res.render('view-library', { files: rows, libraryName: libraryName })
+                        })
+                    })   
+                } else {
+                    req.flash('failure', 'Invalid password')
+                    return res.redirect('/libraries/' + req.params.id)
+                }
+            })
+        }
     })
 })
 
